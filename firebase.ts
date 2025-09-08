fd8c37c66b3852f10a51211ce763f9d85a6a8642
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-09-07 23:19:45 -0400

    Final Commit, done with project

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 //new import -> for setting up authentication 
 import { getAuth } from 'firebase/auth';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
